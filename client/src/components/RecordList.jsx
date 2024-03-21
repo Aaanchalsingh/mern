2629627715fc570a8e2293bfbd697d6e2a842895
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-03-21 12:07:25 +0530

    commit

--- client/src/components/RecordList.jsx
+++ client/src/components/RecordList.jsx
@@ -56,7 +56,7 @@ export default function RecordList() {
 
   // This method will delete a record
   async function deleteRecord(id) {
-    await fetch(`https://mern-backend-5m397p0ow-aanchal-singhs-projects.vercel.app/${id}`, {
+    await fetch(`https://mern-backend-5m397p0ow-aanchal-singhs-projects.vercel.app${id}`, {
       method: "DELETE",
     });
     const newRecords = records.filter((el) => el._id !== id);
